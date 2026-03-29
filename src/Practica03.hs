59941e7f24478ad7ec17f440c8327441a361bc6d
Emily Zarate <emily.zarate.1802@gmail.com>
2026-03-28 22:44:14 -0600

    juancho el oso se equivoco una vez pero no se puede equivocar 2 veces y es la vencida, subira bien su commit

--- src/Practica03.hs
+++ src/Practica03.hs
@@ -49,11 +49,30 @@ type Clausula = [Literal]
 
 --Ejercicio 1
 clausulas :: Prop -> [Clausula]
-clausulas = undefined
+clausulas p = [quitarRepetidos c | c <- auxClausulas (fnc p)]
+  where
+    auxClausulas (And p1 p2) = auxClausulas p1 ++ auxClausulas p2
+    auxClausulas p1 = [obtenerLiterales p1]
 
 --Ejercicio 2
 resolucion :: Clausula -> Clausula -> Clausula
-resolucion = undefined
+resolucion c1 c2 =
+  let pares = [(l1, l2) | l1 <- c1, l2 <- c2, esComplemento l1 l2]
+   in if esVacia pares
+        then quitarRepetidos (c1 ++ c2)
+        else
+          let (comp1, comp2) = cabeza pares
+           in quitarRepetidos (quitar comp1 c1 ++ quitar comp2 c2)
+  where
+    esComplemento (Var p) (Not (Var q)) = p == q
+    esComplemento (Not (Var p)) (Var q) = p == q
+    esComplemento _ _ = False
+
+    quitar _ [] = []
+    quitar l (x : xs) = if l == x then xs else x : quitar l xs
+
+    quitarRepetidos [] = []
+    quitarRepetidos (x : xs) = x : quitarRepetidos (filtrar (/= x) xs)
 
 {-
 ALGORITMO DE SATURACION
@@ -67,3 +86,22 @@ hayResolvente = undefined
 --Funcion principal que pasa la formula proposicional a fnc e invoca a res con las clausulas de la formula.
 saturacion :: Prop -> Bool
 saturacion = undefined
+
+
+--Funciones auxiliares 
+obtenerLiterales (Or p1 p2) = obtenerLiterales p1 ++ obtenerLiterales p2
+obtenerLiterales l = [l]
+
+filtrar :: (a -> Bool) -> [a] -> [a]
+filtrar _ [] = []
+filtrar p (x:xs)
+  | p x       = x : filtrar p xs
+  | otherwise = filtrar p xs
+
+cabeza :: [a] -> a
+cabeza (x:_) = x
+cabeza []    = error "Lista vacia"
+
+esVacia :: [a] -> Bool
+esVacia [] = True
+esVacia _  = False
